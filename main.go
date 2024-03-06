1bff738b2753a0853d082909481f90369fa554f5
Micah Cowell <micahcowell@gmail.com>
2024-03-05 17:23:37 -0800

    moved main.go to root

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/micahco/musli/internal/musli"
+)
+
+func main() {
+	configFile, err := musli.GetDefaultConfigPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var flagC string
+	usageC := "use config at <path>"
+	flag.StringVar(&flagC, "c", configFile, usageC)
+	flag.StringVar(&flagC, "config", configFile, usageC)
+
+	var flagQ string
+	usageQ := "find albums with name or artist that contains <query>"
+	flag.StringVar(&flagQ, "q", "", usageQ)
+	flag.StringVar(&flagQ, "query", "", usageQ)
+
+	var flagR bool
+	usageR := "list random albums"
+	flag.BoolVar(&flagR, "r", false, usageR)
+	flag.BoolVar(&flagR, "random", false, usageR)
+
+	var flagS bool
+	usageS := "scan music directory to database"
+	flag.BoolVar(&flagS, "s", false, usageS)
+	flag.BoolVar(&flagS, "scan", false, usageS)
+
+	var flagY string
+	usageY := "find albums from <year> or range <year-end>"
+	flag.StringVar(&flagY, "y", "", usageY)
+	flag.StringVar(&flagY, "year", "", usageY)
+
+	flag.Usage = func() {
+		u := `Usage of musli:
+ -c, --config <path>: %s
+ -q, --query <query>: %s
+ -r, --random: %s
+ -s, --scan: %s
+ -y, --year <year(-end)>: %s
+`
+		f := fmt.Sprintf(u, usageC, usageQ, usageR, usageS, usageY)
+		fmt.Print(f)
+	}
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return
+	}
+
+	conf, db, err := musli.Init(flagC)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := musli.CloseDB(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if len(flagQ) > 0 {
+		albums, err := musli.FindAlbumsByNameOrAlbumArtist(flagQ, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(albums) == 0 {
+			fmt.Println("musli: no results")
+			return
+		}
+		err = musli.ListAlbums(albums, conf, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if flagR {
+		albums, err := musli.RandomAlbums(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(albums) == 0 {
+			fmt.Println("musli: no results")
+			return
+		}
+		err = musli.ListAlbums(albums, conf, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if flagS {
+		fmt.Println("Scanning library...")
+		err = musli.ScanLibraryToDB(conf, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if len(flagY) > 0 {
+		albums, err := musli.FindAlbumsByYear(flagY, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(albums) == 0 {
+			fmt.Println("musli: no results")
+			return
+		}
+		err = musli.ListAlbums(albums, conf, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+}
